65cda9710a27390cce6ffbe90e9de578f825d17c
NANONANDFLASH <68502584+NANONANDFLASH@users.noreply.github.com>
2023-07-14 00:40:49 +0900

    study

--- URL Checker/main.go
+++ URL Checker/main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("Request failed")
+
 func main() {
-	
+	var results = make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	for _, url := range urls{
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "failed"
+		}
+		results[url] = result
+	}
+	for url, result := range results{
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checkin: ", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	fmt.Println(err, resp.StatusCode)
+	return nil
 }
